7a1a5d1e676f212b185a7a86c02bb4e61c540257
The-Majestex <williamsehizibolo@gmail.com>
2025-08-28 20:41:26 +0100

    Day 2: added forex data tech

--- PipVision.py
+++ PipVision.py
@@ -1,1 +1,7 @@
 print("PipVision starting.........")
+import yfinance as yf
+# I am getting EURUSD Forex pair 
+data = yf.download("EURUSD=X", period="1d", interval="15m")
+print("EUR/USD 15m data:")
+print(data.tail()) 
+
